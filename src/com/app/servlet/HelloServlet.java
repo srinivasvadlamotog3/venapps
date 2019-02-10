24045c3be126ae660012e41166e4bd7ac70c3121
user <user@user-PC>
2019-02-10 20:11:28 +0530

    commit-1 successfullt

---
+++ src/com/app/servlet/HelloServlet.java
@@ -0,0 +1,21 @@
+package com.app.servlet;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+
+import javax.servlet.GenericServlet;
+import javax.servlet.ServletException;
+import javax.servlet.ServletRequest;
+import javax.servlet.ServletResponse;
+
+public class HelloServlet  extends GenericServlet{
+
+	@Override
+	public void service(ServletRequest req, ServletResponse res)
+			throws ServletException, IOException {
+		PrintWriter out=res.getWriter();
+		out.println("hello servlet");
+		
+	}
+
+}
